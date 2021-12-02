39c94c6f01da70ee5bff1f827fb5bdbcd04f90a4
mauduran <is716122@iteso.mx>
2021-12-02 22:25:20 +0100

    Removed test route and controller

--- task-manager-back/src/routes/routes.js
+++ task-manager-back/src/routes/routes.js
@@ -1,9 +1,7 @@
-const TestRoute = require("./test.route");
 const TaskRoute = require("./task.route");
 const UserRoute = require("./user.route");
 
 const Routes = {
-    "test": TestRoute,
     "task": TaskRoute,
     "user": UserRoute,
 }
