13bc5b501737001e5d40e40169c976efe05aa5d4
mauduran <is716122@iteso.mx>
2021-12-04 17:11:58 +0100

    Style changes and snackbar on  app

--- task-manager-front/src/App.js
+++ task-manager-front/src/App.js
@@ -11,18 +11,31 @@ import RequireAuth from './common/RequireAuth';
 import RequireUnAuth from './common/RequireUnAuth';
 import AppHeader from './components/AppHeader/AppHeader';
 import Home from './containers/Home/Home';
+import { Alert, AlertTitle, Snackbar } from '@mui/material';
+import { selectNotificationState } from './redux/notification/notification.selectors';
+import { closeNotification } from './redux/notification/notification.actions';
 
-function App({ checkUserSession, currentUser, signOut }) {
+function App({ checkUserSession, notificationState, closeNotification }) {
   useEffect(() => {
     checkUserSession();
   }, [checkUserSession]);
 
   return (
     <div className="App">
-      <AppHeader/>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        autoHideDuration={3000}
+        onClose={closeNotification}
+        open={notificationState.open} >
+        <Alert onClose={closeNotification} severity={notificationState.severity}>
+          <AlertTitle>{notificationState.title}</AlertTitle>
+          {notificationState.message}
+        </Alert>
+      </Snackbar>
+      <AppHeader />
 
       <Routes>
-        <Route path='/' element={<RequireAuth><Home/></RequireAuth>} />
+        <Route path='/' element={<RequireAuth><Home /></RequireAuth>} />
         <Route path='/task/:id' element={<RequireAuth><h1>Task Detail</h1></RequireAuth>} />
         <Route exact path='/signin' element={<RequireUnAuth> <SignInAndSignUp /> </RequireUnAuth>} />
         <Route path="*" element={<h1>404 -Nothing to see here</h1>} />
@@ -33,9 +46,11 @@ function App({ checkUserSession, currentUser, signOut }) {
 
 const mapDispatchToProps = dispatch => ({
   checkUserSession: () => dispatch(checkUserSession()),
+  closeNotification: () => dispatch(closeNotification()),
 })
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  notificationState: selectNotificationState,
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
