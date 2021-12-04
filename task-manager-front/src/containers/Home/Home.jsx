539d458b4462b9452008eab9c013b5f779d223c1
mauduran <is716122@iteso.mx>
2021-12-04 17:13:30 +0100

    Added styles and refresh button to home

--- task-manager-front/src/containers/Home/Home.jsx
+++ task-manager-front/src/containers/Home/Home.jsx
@@ -1,8 +1,10 @@
+import { Button, Stack } from '@mui/material';
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux';
 import TaskListContainer from '../../components/TaskList/TaskListContainer';
 import { fetchTasksStart } from '../../redux/task/task.actions';
 import { useHomeStyles } from './Home.styles';
+import LoopIcon from '@mui/icons-material/Loop';
 
 
 const Home = ({ fetchTasks }) => {
@@ -13,8 +15,12 @@ const Home = ({ fetchTasks }) => {
     }, [fetchTasks]);
 
     return (
-        <div>
+        <div className={classes.Home}>
             <h1 className={classes.title}>These are your tasks:</h1>
+            <Stack className={classes.actionButtons} direction="row" spacing={1}>
+                <Button variant="contained" color="success">Add Task</Button>
+                <Button variant="contained" startIcon={<LoopIcon />} onClick={fetchTasks}>Refresh</Button>
+            </Stack>
             <div className={classes.HomeContainer}>
                 <TaskListContainer />
             </div>
@@ -23,7 +29,7 @@ const Home = ({ fetchTasks }) => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    fetchTasks: () => { return dispatch(fetchTasksStart())}
+    fetchTasks: () => { return dispatch(fetchTasksStart()) }
 })
 
 export default connect(null, mapDispatchToProps)(Home);
--- task-manager-front/src/containers/Home/Home.styles.js
+++ task-manager-front/src/containers/Home/Home.styles.js
@@ -1,16 +1,26 @@
 import { makeStyles } from '@mui/styles';
 
 export const useHomeStyles = makeStyles((theme) => ({
+    Home: {
+        flex: 1,
+    },
+
     HomeContainer: {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         margin: "0 auto",
-        width: "80%",
+        width: "90%",
+        flex: 1,
     },
     title: {
-        marginLeft: "30px",
+        marginLeft: "5%",
         fontSize: "48px",
+    },
+
+    actionButtons: {
+        margin: "30px 0",
+        marginLeft: "5%",
     }
 
 }));
