1962271261497537fbe157a67597a8116eed01af
mauduran <is716122@iteso.mx>
2021-12-05 01:37:10 +0100

    Modified require unAuth to return user to desired path

--- task-manager-front/src/common/RequireUnAuth.jsx
+++ task-manager-front/src/common/RequireUnAuth.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from '../redux/user/user.selectors';
 
-const RequireUnAuth = ({currentUser, children}) => {
-    return (currentUser)? <Navigate to="/" /> :  children;
+const RequireUnAuth = ({ currentUser, children }) => {
+    const location = useLocation();
+
+    let to = (location.state && location.state.from && location.state.from.pathname) ? location.state.from.pathname : "/";
+
+    return (currentUser) ? <Navigate to={to} /> : children;
 }
 
 const mapStateToProps = createStructuredSelector({
