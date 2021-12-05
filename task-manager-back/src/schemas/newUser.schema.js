f65916f669e0c5738cb8347bd442d4edd8380fbd
mauduran <is716122@iteso.mx>
2021-12-05 22:20:39 +0100

    Removed length validations for user to immprove usability

--- task-manager-back/src/schemas/newUser.schema.js
+++ task-manager-back/src/schemas/newUser.schema.js
@@ -2,8 +2,8 @@ const yup = require("yup");
 
 let newUserSchema = yup.object().shape({
     name: yup.string().required().trim(),
-    username: yup.string().required().min(5),
-    password: yup.string().required().min(5).max(64),
+    username: yup.string().required(),
+    password: yup.string().required(),
 }).noUnknown();
 
 module.exports = newUserSchema;
